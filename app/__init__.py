b68f68c0f3878b7c1539c600774495ad0aef0a69
Shahzaib70456 <mh7024@nyu.edu>
2026-03-05 15:55:43 +0400

    feat(S1): register auth namespace in Flask-RESTX API

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,20 @@
-
 from app.config import Config
 from app.db import DB
+from werkzeug.exceptions import HTTPException
 
 from http import HTTPStatus
 from flask import Flask
 from flask_restx import Api
 from flask_jwt_extended import JWTManager  
 from app.apis.classes import api as classes_ns  
+from app.apis.auth import api as auth_ns
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     DB.init_app(app)
-    JWTManager(app)   
+    jwt = JWTManager(app)   
 
     api = Api(
         title="Fitness Class Management API",
@@ -29,12 +31,25 @@ def create_app():
         security='Bearer',
     )
 
-
     api.init_app(app)
     api.add_namespace(classes_ns) 
+    api.add_namespace(auth_ns) 
+
+    from flask_jwt_extended.exceptions import JWTExtendedException
+    from jwt.exceptions import PyJWTError
+
+    @api.errorhandler(JWTExtendedException)
+    def handle_jwt_exceptions(error):
+        return {"message": str(error)}, HTTPStatus.BAD_REQUEST
+
+    @api.errorhandler(PyJWTError)
+    def handle_pyjwt_exceptions(error):
+        return {"message": str(error)}, HTTPStatus.BAD_REQUEST
 
     @api.errorhandler(Exception)
     def handle_input_validation_error(error):
+        if isinstance(error, HTTPException):
+            return {"message": getattr(error, 'data', {}).get('message', str(error))}, error.code
         return {"message": str(error)}, HTTPStatus.INTERNAL_SERVER_ERROR
 
     return app
